a508a7f5de1c214bb381a73bdcf2df19bf5e5f2f
Ch3LL <megan.wilhite@gmail.com>
2018-12-07 14:03:35 -0500

    Add more multi-master failover tests and add xfail decorator till issue fixed

--- tests/test_multi_master_failover.py
+++ tests/test_multi_master_failover.py
@@ -103,10 +103,11 @@ def test_multi_master_failover_stop_second_master(start_multi_master):
     ret = master1.exec_run('salt * test.ping')
     assert b'The master is not responding' in ret.output
 
+# marking xfail until this issue is fixed: 50791
+@pytest.mark.xfail
 def test_multi_master_failover_restart_both_masters(start_multi_master):
     '''
-    test multi-master failover when first master is running
-    and second is not on startup of minion
+    test multi-master failover when both masters are restarted
     '''
     master1 = docker_client('master1')
     master2 = docker_client('master2')
@@ -129,5 +130,55 @@ def test_multi_master_failover_restart_both_masters(start_multi_master):
     time.sleep(100)
 
     ret = master1.exec_run('salt * test.ping')
+    assert ret.exit_code == 0
+    assert b'True' in ret.output
+
+def test_multi_master_failover_stop_first_master(start_multi_master):
+    '''
+    test multi-master failover when first master is
+    down on startup of minion
+    '''
+    # stop master1 and restart minion1
+    master1 = docker_client('master1')
+    master1.exec_run('pkill salt-master')
+    minion1 = docker_client('minion1')
+    minion1.exec_run('pkill salt-minion')
+    minion1.exec_run('salt-minion -d')
+    time.sleep(40)
+
+    master2 = docker_client('master2')
+    ret = master2.exec_run('salt * test.ping')
+    assert ret.exit_code == 0
+    assert b'True' in ret.output
+
+    ret = master1.exec_run('salt * test.ping')
+    assert b'The master is not responding' in ret.output
+
+# marking xfail until this issue is fixed: 50791
+@pytest.mark.xfail
+def test_multi_master_failover_masters_stopped_on_start(start_multi_master):
+    '''
+    test multi-master failover when both masters are stopped
+    on start of minion. later on start master and make sure minion
+    re-connects
+    '''
+    # stop master1 and restart minion1
+    master1 = docker_client('master1')
+    master2 = docker_client('master2')
+    minion1 = docker_client('minion1')
+    for x,y in {master1: 'master', master2: 'master', minion1: 'minion'}.items():
+        x.exec_run('pkill salt-{0}'.format(y))
+
+    # start minion
+    minion1.exec_run('salt-minion -d')
+    time.sleep(200)
+
+    for master in [master1, master2]:
+        ret = master.exec_run('salt * test.ping')
+        assert b'The master is not responding' in ret.output
+
+    master2.exec_run('salt-master -d')
+    time.sleep(40)
+    ret = master2.exec_run('salt * test.ping')
     assert ret.exit_code == 0
     assert b'True' in ret.output
